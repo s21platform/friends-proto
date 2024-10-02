cbf4fc736690fcd9b04e1de1b89c8f3d64b80288
asepopov <asepopov@avito.ru>
2024-10-02 16:52:35 +0300

    add message for kafka (new_user_register)

--- generate.sh
+++ generate.sh
@@ -3,3 +3,7 @@ protoc  --go_out=./  \
         friends.proto
 
 protoc --doc_out=. --doc_opt=markdown,README.md ./friends.proto
+
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
+        new_user.proto
